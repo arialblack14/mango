687e131e5ae2bfe11799629c55501cd5428701f6
Alex Tsiras <tsirasalexis@yahoo.gr>
2020-08-02 16:14:15 +0300

    Update test_helper file

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,4 +2,5 @@ ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(Mango.Repo, :manual)
 
 {:ok, _} = Application.ensure_all_started(:wallaby)
+Application.put_env(:wallaby, :screenshot_on_failure, true)
 Application.put_env(:wallaby, :base_url, MangoWeb.Endpoint.url())
