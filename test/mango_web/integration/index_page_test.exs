358948cc75ccc0f8b5891017fb3f72e6649109a2
Alex Tsiras <tsirasalexis@yahoo.gr>
2020-08-02 16:14:34 +0300

    Add acceptance tests

--- test/mango_web/integration/index_page_test.exs
+++ test/mango_web/integration/index_page_test.exs
@@ -1,12 +1,27 @@
 defmodule MangoWeb.IndexPageTest do
   use MangoWeb.IntegrationCase, async: true
+  use Wallaby.Feature
 
-  import Wallaby.Query, only: [css: 2]
+  import Wallaby.Query, only: [css: 2, text_field: 1, button: 1]
+
+  def index_title(title), do: css("h1", text: title)
 
   test "Has an index page", %{session: session} do
     session
     |> visit("/")
     |> find(css(".navbar-nav", count: 1))
     |> assert_has(css("a", text: "Link"))
+  end
+
+  feature "presence of featured products", %{session: session} do
+    session
+    |> visit("/")
+    |> assert_has(css("h1", text: "Seasonal products"))
+  end
+
+  feature "index page has displays products with their names", %{session: session} do
+    session
+    |> visit("/")
+    |> assert_has(index_title("Seasonal products"))
   end
 end
